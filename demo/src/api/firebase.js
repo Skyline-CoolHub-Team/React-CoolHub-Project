7d8e8df19ad4fe4b188f163491188e9bd8bb0736
raszxcv <282975975@qq.com>
2017-04-21 15:00:55 +0800

    回退

---
+++ demo/src/api/firebase.js
@@ -0,0 +1,12 @@
+/**
+ * firebase config
+ */
+const config = {
+  apiKey: "AIzaSyAzNIcauGMf_V_N7_BY4Hl7jrhYZv5XzWQ",
+  authDomain: "react-coolhub.firebaseapp.com",
+  databaseURL: "https://react-coolhub.firebaseio.com",
+  projectId: "react-coolhub",
+  storageBucket: "react-coolhub.appspot.com",
+  messagingSenderId: "205032541101"
+}
+
