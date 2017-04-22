dc312ba17302dc3f5175cd16d1635e662c49217e
skyline <rubikandme@outlook.com>
2017-04-23 05:51:07 +0800

    change the route_code

--- coolhub/src/pages/code/code.js
+++ coolhub/src/pages/code/code.js
@@ -1,11 +1,9 @@
 import React from 'react'
 // componnets
-import CodeAppBar from './code_app_bar'
 import CodeLists from './code_lists'
 
 const Code = () => (
   <div>
-    <CodeAppBar />
     <CodeLists />
   </div>
 )
--- coolhub/src/route/route_code.js
+++ coolhub/src/route/route_code.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import { Route } from 'react-router-dom'
 // components
+import CodeAppBar from '../pages/code/code_app_bar'
 import Code from '../pages/code/code'
 import File from '../pages/readme/readme'
 
 const CodeRoute = (props) => (
     <div>
+      <CodeAppBar />
       <Route exact path='/' component={ Code } />
       {/*<Route path={`${props.match.url}/:owner?/:repo`} component={ Readme } />*/}
     </div>
